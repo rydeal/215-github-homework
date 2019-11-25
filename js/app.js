539e144c4a9cb5b246cabcdb41b73e6ef910fd89
Ryan Deal <rydeal@iu.edu>
2019-11-25 01:48:51 -0500

    Completed the website

--- js/app.js
+++ js/app.js
@@ -1,22 +1,9 @@
-var visibleDiv = 0;
-var lastDiv = 0;
-var theDiv = null;
-function showDiv(click) {
-    theDiv = document.getElementById(click).innerHTML;
-    console.log(theDiv);
-    theDiv.style.display = theDiv.style.display == "none" ? "flex" : "none";
-    // for(x = 0; x < 16; x++) {
-    //     if (theDiv == x) {
-    //         if(visibleDiv == 0 && x !== lastDiv) {
-    //             document.getElementsByClassName("eq" + theDiv + "-text")[0].style.backgroundColor = "white";
-    //             visibleDiv = 1;
-    //             lastDiv = x;
-    //         } else {
-    //             changeDiv = document.getElementsByClassName("eq" + theDiv + "-text");
-    //             console.log(changeDiv);
-    //             visibleDiv = 0;
-    //             lastDiv = x;
-    //         }  
-    //     }
-    // }
+//simple function to determine if the details are visible or not
+function showDiv(clicked) {
+    divMod = document.getElementById(clicked);
+    if(divMod.style.display === "none") {
+        divMod.style.display = "block";
+    } else {
+        divMod.style.display = "none";
+    }
 }
